0c51fd703543149474518f0d542f7f2e3efe2ad6
Dean Leibner <Dean.Leibner@avl.com>
2018-11-13 19:55:10 +0100

    exercise 02

--- exercise/02/array.cpp
+++ exercise/02/array.cpp
@@ -1,1 +1,82 @@
 #include "array.h"
+#include <exception>
+#include <stdexcept>
+
+array::array() :
+  p(nullptr), n(0)
+{
+}
+
+array::array(const int size, const double value)
+{
+  if (size > 0)
+  {
+    n = size;
+    p = new double[size];
+
+    for (int i = 0; i < size; ++i)
+    {
+      p[i] = value;
+    }
+  }
+  else
+  {
+    n = 0;
+    p = nullptr;
+  }
+}
+
+array::array(const array& other) :
+  p(nullptr), n(0)
+{
+  n = other.n;
+
+  if (n > 0)
+  {
+    p = new double[n];
+
+    for (int i = 0; i < n; ++i)
+    {
+      p[i] = other.p[i];
+    }
+  }
+}
+
+array::array(array&& other) noexcept :
+  p(nullptr), n(0)
+{
+  std::swap(p, other.p);
+  std::swap(n, other.n);
+}
+
+array::~array()
+{
+  if (p)
+  {
+    delete[] p;
+    p = nullptr;
+  }
+}
+
+int array::size() const
+{
+  return n;
+}
+
+double array::at(const int index) const
+{
+  if (index < 0 || index >= n)
+  {
+    throw std::out_of_range("index is out of range");
+  }
+
+  return p[index];
+}
+
+array& array::operator=(array other)
+{
+  std::swap(p, other.p);
+  std::swap(n, other.n);
+
+  return *this;
+}
--- exercise/02/array.h
+++ exercise/02/array.h
@@ -1,7 +1,20 @@
+#pragma once
 // do not use standard container as member or base class
 class array
 {
-	double* p;
-	int n;
 public:
+  array();
+  array(int size, double value = 0);
+  array(const array& other);
+  array(array&& other) noexcept;
+  ~array();
+
+  int size() const;
+  double at(int index) const;
+
+  array& operator=(array other);
+
+private:
+  double* p;
+  int n;
 };
